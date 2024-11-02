d856b393cb30ee6201db438bbf739e39ac95dfcc
Joralmo <joralmopro@gmail.com>
2024-11-02 11:36:44 -0500

    Make it better :fire:

---
+++ src/App.jsx
@@ -0,0 +1,58 @@
+import "./App.css";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  Navigate,
+} from "react-router-dom";
+import { Registro } from "./components/registro";
+import { IniciarSesion } from "./components/iniciarSesion";
+import { NuevoPost } from "./components/nuevoPost";
+import { ListaDePosts } from "./components/listaDePosts";
+
+function App() {
+  const isAuthenticated = () => {
+    return localStorage.getItem("user") !== null;
+  };
+
+  const PrivateRoute = ({ children }) => {
+    return isAuthenticated() ? children : <Navigate to="/login" />;
+  };
+
+  const logout = () => {
+    localStorage.removeItem("user");
+    window.location.href = "/login";
+  };
+
+  return (
+    <Router>
+      {isAuthenticated && (
+        <button className="logout" onClick={logout}>
+          Cerrar sesión
+        </button>
+      )}
+      <Routes>
+        <Route path="/register" element={<Registro />} />
+        <Route path="/login" element={<IniciarSesion />} />
+        <Route
+          path="/nuevoPost"
+          element={
+            <PrivateRoute>
+              <NuevoPost />
+            </PrivateRoute>
+          }
+        />
+        <Route
+          path="/listaDePosts"
+          element={
+            <PrivateRoute>
+              <ListaDePosts />
+            </PrivateRoute>
+          }
+        />
+      </Routes>
+    </Router>
+  );
+}
+
+export default App;
